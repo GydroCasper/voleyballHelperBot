6e56661df9712350c82bef42d73f5060e5ed8d40
GydroCasper <gydrocasper@outlook.com>
2023-07-04 14:44:57 -0400

    add express endpoint to pass healthcheck

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const vision = await import("@google-cloud/vision")
 
 dotenv.config()
 
+import express from 'express'
+const app = express()
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+})
+
 
 // const logFile = fs.createWriteStream("log.txt", { flags: "a" })
 // const logStdout = process.stdout
